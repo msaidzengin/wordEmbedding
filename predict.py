f07f7fc7e8e599c91d0df3ef818552e96ea051a7
msaidzengin <37325455+msaidzengin@users.noreply.github.com>
2021-02-16 15:50:02 +0300

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,5 +3,9 @@ from gensim.models import Word2Vec
 from gensim.models import KeyedVectors
 
 model = KeyedVectors.load_word2vec_format('wordEmbeddings.txt', binary=False)
-vector = model.wv['ve']
-print(vector)
+print(model.most_similar("aselsan")) # prints tusaş, roketsan, havelsan, tai, ssb ...
+print(model.most_similar(positive=['silah','uçak'],negative=['araba'],topn=1)) # prints iha
+print(model.most_similar(positive=['silah','uçak'],negative=['iha'],topn=1)) # prints muharebe
+print(model.similarity('roket','silah')) # 0.7742
+print(model.most_similar('motor')) # dizel, pompa, servo, döner, valf
+print(model.most_similar('füze')) # roket, fırlatma, denizaltı,
